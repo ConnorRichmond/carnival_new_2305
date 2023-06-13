8bb71ac0d8a380982dae77ea84c0f02d517d479a
Connor Richmond <richmond.connor@gmail.com>
2023-06-13 12:05:31 -0400

    Add methods to log riders and take revenue

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,5 @@
 class Ride
-  
+
   attr_reader :name, :min_height, :admission_fee, :excitement, :rider_log
 
   def initialize(attributes)
@@ -8,6 +8,18 @@ class Ride
     @admission_fee = attributes[:admission_fee]
     @excitement = attributes[:excitement]
     @rider_log = {}
+  end
+
+  def board_rider(visitor)
+    if visitor.tall_enough?(@min_height) && visitor.preferences.include?(@excitement)
+      visitor.spend_money(@admission_fee)
+      @rider_log[visitor] ||= 0
+      @rider_log[visitor] += 1
+    end
+  end
+
+  def total_revenue
+    @rider_log.values.sum * @admission_fee
   end
 
 end
