e584dac8a705eabdd8da1590c7c58e741d9ec7cc
Connor Richmond <richmond.connor@gmail.com>
2023-06-13 12:50:18 -0400

    Test for most popular ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -4,6 +4,12 @@ describe Carnival do
 
   before(:each) do
     @witchlight = Carnival.new(6)
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
   describe '#initialize' do
@@ -11,7 +17,27 @@ describe Carnival do
       expect(@witchlight).to be_a(Carnival)
       expect(@witchlight.rides).to eq([])
     end
+
+    it 'has a most popular ride' do
+      @visitor1.add_preference(:gentle)
+      @visitor2.add_preference(:gentle)
+      @visitor2.add_preference(:thrilling)
+      @visitor3.add_preference(:thrilling)
+      @witchlight.add_ride(@ride1)
+      @witchlight.add_ride(@ride2)
+      @witchlight.add_ride(@ride3)
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor3)
+      @ride2.board_rider(@visitor2)
+      @ride2.board_rider(@visitor3)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@witchlight.most_popular_ride).to eq(@ride1)
+    end
   end
+
+
 
 
 end
