8ecd1ed2d32ff002d3765b6450be6a4876bb2468
Connor Richmond <richmond.connor@gmail.com>
2023-06-13 11:26:09 -0400

    Initialization for Visitor Class

---
+++ lib/visitor.rb
@@ -0,0 +1,15 @@
+class Visitor
+
+  attr_reader :name,
+              :spending_money,
+              :height,
+              :preferences
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money
+    @preferences = []
+  end
+
+end
