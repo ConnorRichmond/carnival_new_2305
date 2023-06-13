e1cc80313256ae2e7eeaf58fed3f544bb3cfe985
Connor Richmond <richmond.connor@gmail.com>
2023-06-13 12:26:31 -0400

    Test: hight and preference limitations

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -20,8 +20,11 @@ describe Ride do
   describe "logs ride and calculates revenue" do
     before(:each) do
       @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
       @visitor1 = Visitor.new('Bruce', 54, '$10')
       @visitor2 = Visitor.new('Tucker', 36, '$5')
+      @visitor3 = Visitor.new('Penny', 64, '$15')
     end
 
     it 'logs riders that board a ride and takes money' do
@@ -32,7 +35,7 @@ describe Ride do
       @ride1.board_rider(@visitor1)
       @ride1.board_rider(@visitor2)
       @ride1.board_rider(@visitor1)
-      
+
       expect(@ride1.rider_log).to eq({
         @visitor1 => 2,
         @visitor2 => 1
@@ -41,6 +44,32 @@ describe Ride do
       expect(@visitor2.spending_money).to eq("$4")
 
       expect(@ride1.total_revenue).to eq(3)
+    end
+
+    it 'only logs riders who are tall enough' do
+      @visitor1.add_preference(:gentle)
+      @visitor2.add_preference(:gentle)
+      @visitor2.add_preference(:thrilling)
+      @visitor3.add_preference(:thrilling)
+      expect(@visitor1.preferences).to eq([:gentle])
+      expect(@visitor2.preferences).to eq([:gentle, :thrilling])
+      expect(@visitor3.preferences).to eq([:thrilling])
+
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@visitor1.spending_money).to eq('$8')
+      expect(@visitor2.spending_money).to eq('$4')
+      expect(@visitor3.spending_money).to eq('$13')
+
+      expect(@ride3.rider_log).to eq({
+        @visitor3 => 1
+      })
+      expect(@ride3.total_revenue).to eq(2)
     end
   end
 
