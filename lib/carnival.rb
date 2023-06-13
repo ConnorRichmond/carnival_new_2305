524106919ffe31884597a2b698971fc1f43038af
Connor Richmond <richmond.connor@gmail.com>
2023-06-13 13:06:47 -0400

    Add methods for most profitable ride+total revenue

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -15,4 +15,16 @@ class Carnival
     @rides.max_by { |ride| ride.total_riders }
   end
 
+  def most_profitable_ride
+    @rides.max_by { |ride| ride.total_revenue }
+  end
+
+  def carnival_revenue
+    total = 0
+    @rides.each do |ride|
+      total += ride.total_revenue
+    end
+    total
+  end
+
 end
